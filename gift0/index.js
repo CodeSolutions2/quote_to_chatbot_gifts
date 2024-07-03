37a059ac99890d70a015c63ec0f1954246c0dfd3
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-07-03 14:28:42 +0200

    Update index.js

--- gift0/index.js
+++ gift0/index.js
@@ -1,2 +1,2 @@
-import { encrypted_CRUD_file_database } from "./library_window_crypto_subtle_class.js";
+import { encrypted_CRUD_file_database } from "./gift0/library_window_crypto_subtle_class.js";
 window.encrypted_CRUD_file_database = encrypted_CRUD_file_database;
